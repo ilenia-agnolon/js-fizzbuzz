35979b6e7d319d48a1f1de6bd3aad9c171e8fa17
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-08-28 16:03:31 +0200

    corretto errore di digitatura (mancava il "<") nella fase INPUT, altriemnti la pagina sarebbe andata in loop per sempre

--- script.js
+++ script.js
@@ -11,22 +11,30 @@ ________________________________________________________________________________
 //1. INPUT
 
 // • chiedo al programma di stampare i numeri da 1 a 100
-    for (let i =1; i =100; i++) {
-
+    for (let i =1; i <= 100; i++) {
 
 //___________________________________________________________________________________________
 
 //2. ELABORAZIONE
 
+// • indico che per i numeri multipli sia di 3 che di 5 deve venire stampato FizzBuzz al posto del numero
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz", i);
+    }
+
 // • indico che per i numeri multipli di 3 deve venire stampato Fizz al posto del numero
-    if (i % 3 === 0) { console.log("Fizz", i); }
+    else if (i % 3 === 0) {
+        console.log("Fizz", i);
+    }
 
 
 // • indico che per i numeri multipli di 5 deve venire stampato Buzz al posto del numero
-    else if (i % 5 === 0) { console.log("Buzz", i); }
+    else if (i % 5 === 0) { 
+        console.log("Buzz", i); }
     }
+    
 
-// • indico che per i numeri multipli sia di 3 che di 5 deve venire stampato FizzBuzz al posto del numero
+
 
 // • altrimenti stampo il numero intero
 
