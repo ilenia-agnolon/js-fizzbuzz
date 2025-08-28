f10db504b4fd1339f6ed2ba50d655d3c0f4af08b
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-08-28 15:24:22 +0200

    inserito passaggi logici

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-/* Scrivi un programma che stampi i numeri da 1 a 100,
+/*___________________________________________________________________________________________
+Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+___________________________________________________________________________________________*/
+
+
+
+//PASSAGGI LOGICI:
+
+//1. INPUT
+
+// • chiedo al programma di stampare i numeri da 1 a 100
+
+
+//___________________________________________________________________________________________
+
+//2. ELABORAZIONE
+
+// • indico che per i numeri multipli di 3 deve venire stampato Fizz al posto del numero
+
+// • indico che per i numeri multipli di 5 deve venire stampato Buzz al posto del numero
+
+// • indico che per i numeri multipli sia di 3 che di 5 deve venire stampato FizzBuzz al posto del numero
+
+// • altrimenti stampo il numero intero
+
+//___________________________________________________________________________________________
+
+//3. OUTPUT
+
+// • stampare in console i numeri da 1 a 100 con l'indicazione per i multipli di 3 e 5
